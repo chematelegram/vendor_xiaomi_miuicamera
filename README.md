7a53a8c3886f02182271cb98a3ab13d7a895718e
chematelegram <chfondia@gmail.com>
2024-01-02 01:48:54 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,23 @@
+# Leica + MIUI Camera for Mi 9T/Redmi K20 (davinci) AOSP 13
+
+### Cloning :
+- Clone this repo in vendor/xiaomi/miuicamera in your working directory by :
+```
+git clone https://gitlab.com/ItzDFPlayer/vendor_davinci-miuicamera -b leica+miui vendor/xiaomi/miuicamera
+```
+
+Make these changes in **sm6150-common**
+
+**sm6150.mk**
+```
+# MiuiCamera
+$(call inherit-product, vendor/xiaomi/miuicamera/config.mk)
+```
+## Credits
+
+### Original mod - https://github.com/a406010503/Miui_Camera
+
+## Support
+
+### https://t.me/itzdfplayer_stash <br>
+
---
+++ config.mk
@@ -0,0 +1,26 @@
+PRODUCT_SOONG_NAMESPACES += \
+    vendor/xiaomi/miuicamera
+
+PRODUCT_COPY_FILES += \
+    $(call find-copy-subdir-files,*,vendor/xiaomi/miuicamera/proprietary/system/etc,$(TARGET_COPY_OUT_SYSTEM)/etc) \
+    $(call find-copy-subdir-files,*,vendor/xiaomi/miuicamera/proprietary/system/lib64,$(TARGET_COPY_OUT_SYSTEM)/lib64) \
+    $(call find-copy-subdir-files,*,vendor/xiaomi/miuicamera/proprietary/system/priv-app/MiuiCamera/lib,$(TARGET_COPY_OUT_SYSTEM)/priv-app/MiuiCamera/lib) \
+    $(call find-copy-subdir-files,*,vendor/xiaomi/miuicamera/proprietary/system/priv-app/MiuiExtraPhoto/lib,$(TARGET_COPY_OUT_SYSTEM)/priv-app/MiuiExtraPhoto/lib) \
+    $(call find-copy-subdir-files,*,vendor/xiaomi/miuicamera/proprietary/vendor/bin,$(TARGET_COPY_OUT_VENDOR)/bin) \
+    $(call find-copy-subdir-files,*,vendor/xiaomi/miuicamera/proprietary/vendor/etc,$(TARGET_COPY_OUT_VENDOR)/etc) \
+    $(call find-copy-subdir-files,*,vendor/xiaomi/miuicamera/proprietary/vendor/lib,$(TARGET_COPY_OUT_VENDOR)/lib) \
+    $(call find-copy-subdir-files,*,vendor/xiaomi/miuicamera/proprietary/vendor/lib64,$(TARGET_COPY_OUT_VENDOR)/lib64) \
+    $(call find-copy-subdir-files,*,vendor/xiaomi/miuicamera/proprietary/vendor/camera,$(TARGET_COPY_OUT_PRODUCT)/vendor_overlay/$(PRODUCT_TARGET_VNDK_VERSION)/camera)
+
+PRODUCT_PACKAGES += \
+    MiuiCamera \
+    MiuiExtraPhoto \
+    MiuiScanner
+
+# Props
+PRODUCT_PRODUCT_PROPERTIES += \
+    ro.com.google.lens.oem_camera_package=com.android.camera
+
+PRODUCT_SYSTEM_PROPERTIES += \
+    persist.vendor.camera.privapp.list=com.android.camera
+
